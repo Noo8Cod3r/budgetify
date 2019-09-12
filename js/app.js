3d4fea15b3f1bb214358dcf9ad03c16821a7137f
Noo8 Cod3r <noo8cod3r@gmail.com>
2019-09-12 15:27:00 -0500

    add HTML elements to DOM

--- js/app.js
+++ js/app.js
@@ -79,12 +79,14 @@ let UIController = (() => {
       
     },
     addListItem: (obj, type) => {
-      let html, newHtml
+      let html, newHtml, element
       // Create HTML String w/Placholder
       if (type === 'inc') {
-        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%/div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        element = DOMStrings.incomeContainer
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       } else if (type === 'exp') {
-        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        element = DOMStrings.expensesContainer
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
 
       // Replace Placholder with Data
@@ -93,6 +95,7 @@ let UIController = (() => {
       newHtml = newHtml.replace('%value%', obj.value)
 
       // Incert HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
     },
 
@@ -125,6 +128,7 @@ let controller = ((budgetCtrl, UICtrl) => {
     // 2. Add item to Budget controller
     newItem = budgetCtrl.addItem(input.type, input.description, input.value)
     // 3. Add new item to UI
+    UICtrl.addListItem(newItem, input.type)
     // 4. Calculate Budget
     // 5. Display Budget
   }
